fb099d5ebf87138992a14ec50dc2e56014200328
OleksandrKuz <oleksandr.kuzmenko@sigma.software>
2022-07-20 19:33:05 +0300

    Initial commit

---
+++ TodoListApp/TodoListAppApi/Controllers/TodoListController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TodoListAppApi.Controllers
+{
+    [ApiController]
+    [Route("/todolist")]
+    public class TodoListController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TodoListTest() 
+        {
+            return Ok("test");
+        }
+    }
+}
